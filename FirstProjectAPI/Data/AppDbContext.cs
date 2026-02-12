86eaa4a07a4e7b3950ee85e5df1d13458eece311
Panagiotis Karapanagiotis <panoskarap2005@gmail.com>
2026-02-12 19:41:53 +0200

    Add project files.

---
+++ FirstProjectAPI/Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FirstProjectAPI.Models; // <-- Για να βλέπει το Product σου
+
+namespace FirstProjectAPI.Data
+{
+    // Αυτή η κλάση ΕΙΝΑΙ η βάση δεδομένων σου
+    public class AppDbContext : DbContext
+    {
+        // Ο κατασκευαστής (Constructor) - χρειάζεται για να περάσουμε ρυθμίσεις αργότερα
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        // Εδώ λέμε: "Θέλω έναν Πίνακα (DbSet) που θα έχει μέσα Products"
+        public DbSet<Product> Products { get; set; }
+    }
+}
